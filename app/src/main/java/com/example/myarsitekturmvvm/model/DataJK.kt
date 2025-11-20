ecaf1781e0619a6a81d116b19326f31932cdaef2
RMadya <kereyoben75@gmail.com>
2025-11-20 15:35:21 +0700

    add dataJK

---
+++ app/src/main/java/com/example/myarsitekturmvvm/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitekturmvvm.model
+
+import com.example.myarsitekturmvvm.R
+
+object DataJK {
+    val Jenisk = listOf(
+        R.string.laki,
+        R.string.wanita
+    )
+}
