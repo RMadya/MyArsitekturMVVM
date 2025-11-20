2aa3f64943ccd169a70d2c0a90544b9337f4b39d
RMadya <kereyoben75@gmail.com>
2025-11-20 17:46:45 +0700

    pemanggilan siswaApp

--- app/src/main/java/com/example/myarsitekturmvvm/MainActivity.kt
+++ app/src/main/java/com/example/myarsitekturmvvm/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyarsitekturmvvmTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
